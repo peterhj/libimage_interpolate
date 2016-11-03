47756853a38f8c8503d7ea348facc9d554cd2804
Peter Jin <peterhaijin@gmail.com>
2016-11-02 23:20:05 -0700

    Improve loop efficiency.

--- src/linear.rs
+++ src/linear.rs
@@ -7,19 +7,19 @@ pub fn interpolate2d_linear_u8sr<R>(in_dim: (usize, usize, usize), in_buf: &[u8]
   let sx = in_w as f32 / out_w as f32;
   let sy = in_h as f32 / out_h as f32;
   for kv in 0 .. out_h {
+    let v = kv as f32 + 0.5;
+    let y = v * sy as f32;
+    let y0 = (y - 0.5).floor().max(0.0) + 0.5;
+    let y1 = (y + 0.5).floor().min((in_h-1) as f32) + 0.5;
+    let ky0 = y0.floor() as usize;
+    let ky1 = y1.floor() as usize;
     for ku in 0 .. out_w {
       let u = ku as f32 + 0.5;
-      let v = kv as f32 + 0.5;
       let x = u * sx as f32;
-      let y = v * sy as f32;
       let x0 = (x - 0.5).floor().max(0.0) + 0.5;
       let x1 = (x + 0.5).floor().min((in_w-1) as f32) + 0.5;
-      let y0 = (y - 0.5).floor().max(0.0) + 0.5;
-      let y1 = (y + 0.5).floor().min((in_h-1) as f32) + 0.5;
       let kx0 = x0.floor() as usize;
       let kx1 = x1.floor() as usize;
-      let ky0 = y0.floor() as usize;
-      let ky1 = y1.floor() as usize;
       for c in 0 .. out_chan {
         let y0_value = if kx0 != kx1 {
           ((x1 - x) * in_buf[c + in_chan * (kx0 + in_w * ky0)] as f32
